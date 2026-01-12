0804c1b0f11c55a4951a3e1ebb88c2cf19403d54
SHAHBAZcoder09 <shahbazgaming1@gmail.com>
2026-01-13 02:55:42 +0800

    refactor: Remove unused SQL statements from User model

--- src/main/java/com/shortproject/model/User.java
+++ src/main/java/com/shortproject/model/User.java
@@ -40,29 +40,3 @@ public class User {
     @Column(nullable = false)
     private LocalDateTime updatedAt = LocalDateTime.now();
 }
-
-
-
-DELETE FROM Bookings
-WHERE booking_id = 5;
-
-
-
-UPDATE Bookings
-SET 
-    start_time = '2023-10-27 16:00:00',
-    end_time = '2023-10-27 17:30:00',
-    purpose = 'Q4 Strategy Planning (Rescheduled)'
-WHERE booking_id = 5;
-
-
-
-
-INSERT INTO Bookings (room_id, user_name, start_time, end_time, purpose)
-VALUES (
-    101, 
-    'John Doe', 
-    '2023-10-27 14:00:00', 
-    '2023-10-27 15:30:00', 
-    'Q4 Strategy Planning'
-);
